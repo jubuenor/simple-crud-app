f777a1381e891f3fb508afd48e2724ea5c4d4b27
Juancho_o <jubuenor@unal.edu.co>
2022-12-29 11:04:59 -0500

    menu

--- simple_crud_front/src/components/Menu/Menu.js
+++ simple_crud_front/src/components/Menu/Menu.js
@@ -1,37 +1,176 @@
 import React from 'react';
 import { Container, Card, Button} from 'react-bootstrap'
 import './Menu.css';
+import {FaSmileBeam, FaMapMarkerAlt, FaRegHeart, FaHeart} from 'react-icons/fa';
 
 function Menu() {
   return (
-    <div className='menu d-flex flex-column justify-content-center'>
-      <Card bg='secondary' border='primary'>
-        <Card.Header>Featured</Card.Header>
+    <div className='menu d-flex flex-column align-items-center'>
+      <Card className='p-3 mb-2 mt-2'>
+        <Container className='row justify-content-between'>
+          <Container className='col-xl-6 col-md-12'>
+            <h5 className='text-start'>Juan Bueno <span>@jubuenor</span></h5>
+            <p className='text-start'>28/17/2022</p>
+          </Container>
+          <Container className='col-xl-5 col-md-11 d-flex justify-content-between me-4'>
+            <div className='d-flex place'> 
+              <div><FaMapMarkerAlt size={30} ></FaMapMarkerAlt></div>
+              
+              <div className='mt-2'>Somewhere over the rainbow!</div>
+            </div>
+            <div>
+              <FaSmileBeam size={30} ></FaSmileBeam>
+            </div>
+          </Container>
+        </Container>
         <Card.Body>
-            <Card.Title>Special title treatment</Card.Title>
             <Card.Text>
-            With supporting text below as a natural lead-in to additional content.
+            I TURN 22 TODAY!!! I feel all the love and couldn’t be more grateful for your birthday wishes 🥹❤️ can’t believe I’m finally allowed to give birth I TURN 22 TODAY!!! I feel all the love and couldn’t be more grateful for your birthday wishes 🥹❤️ can’t believe I’m finally allowed
             </Card.Text>
         </Card.Body>
         <Card.Footer>
-          <Button variant="primary">Go somewhere</Button>
+          <span></span>
+          <Button className='btn-light'>13 <FaRegHeart size={20} ></FaRegHeart></Button>
         </Card.Footer>
       </Card>
-
-      <Card bg='secondary' border='primary'>
-        <Card.Header>Featured</Card.Header>
+      <Card className='p-3 mb-2 mt-2'>
+        <Container className='row justify-content-between'>
+          <Container className='col-xl-6 col-md-12'>
+            <h5 className='text-start'>Juan Bueno <span>@jubuenor</span></h5>
+            <p className='text-start'>28/17/2022</p>
+          </Container>
+          <Container className='col-xl-5 col-md-11 d-flex justify-content-between me-4'>
+            <div className='d-flex place'> 
+              <div><FaMapMarkerAlt size={30} ></FaMapMarkerAlt></div>
+              
+              <div className='mt-2'>Somewhere over the rainbow!</div>
+            </div>
+            <div>
+              <FaSmileBeam size={30} ></FaSmileBeam>
+            </div>
+          </Container>
+        </Container>
         <Card.Body>
-            <Card.Title>Special title treatment</Card.Title>
             <Card.Text>
-            With supporting text below as a natural lead-in to additional content.
+            I TURN 22 TODAY!!! I feel all the love and couldn’t be more grateful for your birthday wishes 🥹❤️ can’t believe I’m finally allowed to give birth I TURN 22 TODAY!!! I feel all the love and couldn’t be more grateful for your birthday wishes 🥹❤️ can’t believe I’m finally allowed
             </Card.Text>
         </Card.Body>
         <Card.Footer>
-          <Button variant="primary">Go somewhere</Button>
+          <span></span>
+          <Button className='btn-light'>13 <FaRegHeart size={20} ></FaRegHeart></Button>
         </Card.Footer>
       </Card>
-
+      <Card className='p-3 mb-2 mt-2'>
+        <Container className='row justify-content-between'>
+          <Container className='col-xl-6 col-md-12'>
+            <h5 className='text-start'>Juan Bueno <span>@jubuenor</span></h5>
+            <p className='text-start'>28/17/2022</p>
+          </Container>
+          <Container className='col-xl-5 col-md-11 d-flex justify-content-between me-4'>
+            <div className='d-flex place'> 
+              <div><FaMapMarkerAlt size={30} ></FaMapMarkerAlt></div>
+              
+              <div className='mt-2'>Somewhere over the rainbow!</div>
+            </div>
+            <div>
+              <FaSmileBeam size={30} ></FaSmileBeam>
+            </div>
+          </Container>
+        </Container>
+        <Card.Body>
+            <Card.Text>
+            I TURN 22 TODAY!!! I feel all the love and couldn’t be more grateful for your birthday wishes 🥹❤️ can’t believe I’m finally allowed to give birth I TURN 22 TODAY!!! I feel all the love and couldn’t be more grateful for your birthday wishes 🥹❤️ can’t believe I’m finally allowed
+            </Card.Text>
+        </Card.Body>
+        <Card.Footer>
+          <span></span>
+          <Button className='btn-light'>13 <FaRegHeart size={20} ></FaRegHeart></Button>
+        </Card.Footer>
+      </Card>
+      <Card className='p-3 mb-2 mt-2'>
+        <Container className='row justify-content-between'>
+          <Container className='col-xl-6 col-md-12'>
+            <h5 className='text-start'>Juan Bueno <span>@jubuenor</span></h5>
+            <p className='text-start'>28/17/2022</p>
+          </Container>
+          <Container className='col-xl-5 col-md-11 d-flex justify-content-between me-4'>
+            <div className='d-flex place'> 
+              <div><FaMapMarkerAlt size={30} ></FaMapMarkerAlt></div>
+              
+              <div className='mt-2'>Somewhere over the rainbow!</div>
+            </div>
+            <div>
+              <FaSmileBeam size={30} ></FaSmileBeam>
+            </div>
+          </Container>
+        </Container>
+        <Card.Body>
+            <Card.Text>
+            I TURN 22 TODAY!!! I feel all the love and couldn’t be more grateful for your birthday wishes 🥹❤️ can’t believe I’m finally allowed to give birth I TURN 22 TODAY!!! I feel all the love and couldn’t be more grateful for your birthday wishes 🥹❤️ can’t believe I’m finally allowed
+            </Card.Text>
+        </Card.Body>
+        <Card.Footer>
+          <span></span>
+          <Button className='btn-light'>13 <FaRegHeart size={20} ></FaRegHeart></Button>
+        </Card.Footer>
+      </Card>
+      <Card className='p-3 mb-2 mt-2'>
+        <Container className='row justify-content-between'>
+          <Container className='col-xl-6 col-md-12'>
+            <h5 className='text-start'>Juan Bueno <span>@jubuenor</span></h5>
+            <p className='text-start'>28/17/2022</p>
+          </Container>
+          <Container className='col-xl-5 col-md-11 d-flex justify-content-between me-4'>
+            <div className='d-flex place'> 
+              <div><FaMapMarkerAlt size={30} ></FaMapMarkerAlt></div>
+              
+              <div className='mt-2'>Somewhere over the rainbow!</div>
+            </div>
+            <div>
+              <FaSmileBeam size={30} ></FaSmileBeam>
+            </div>
+          </Container>
+        </Container>
+        <Card.Body>
+            <Card.Text>
+            I TURN 22 TODAY!!! I feel all the love and couldn’t be more grateful for your birthday wishes 🥹❤️ can’t believe I’m finally allowed to give birth I TURN 22 TODAY!!! I feel all the love and couldn’t be more grateful for your birthday wishes 🥹❤️ can’t believe I’m finally allowed
+            </Card.Text>
+        </Card.Body>
+        <Card.Footer>
+          <span></span>
+          <Button className='btn-light'>13 <FaRegHeart size={20} ></FaRegHeart></Button>
+        </Card.Footer>
+      </Card>
+      <Card className='p-3 mb-2 mt-2'>
+        <Container className='row justify-content-between'>
+          <Container className='col-xl-6 col-md-12'>
+            <h5 className='text-start'>Juan Bueno <span>@jubuenor</span></h5>
+            <p className='text-start'>28/17/2022</p>
+          </Container>
+          <Container className='col-xl-5 col-md-11 d-flex justify-content-between me-4'>
+            <div className='d-flex place'> 
+              <div><FaMapMarkerAlt size={30} ></FaMapMarkerAlt></div>
+              
+              <div className='mt-2'>Somewhere over the rainbow!</div>
+            </div>
+            <div>
+              <FaSmileBeam size={30} ></FaSmileBeam>
+            </div>
+          </Container>
+        </Container>
+        <Card.Body>
+            <Card.Text>
+            I TURN 22 TODAY!!! I feel all the love and couldn’t be more grateful for your birthday wishes 🥹❤️ can’t believe I’m finally allowed to give birth I TURN 22 TODAY!!! I feel all the love and couldn’t be more grateful for your birthday wishes 🥹❤️ can’t believe I’m finally allowed
+            </Card.Text>
+        </Card.Body>
+        <Card.Footer>
+          <span></span>
+          <Button className='btn-light'>13 <FaRegHeart size={20} ></FaRegHeart></Button>
+        </Card.Footer>
+      </Card>
     </div>
+
+    
   )
 }
 
