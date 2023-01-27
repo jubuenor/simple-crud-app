3301b68cd87909633832d6639846118825760a81
jubuenor <jubuenor@unal.edu.co>
2023-01-27 11:59:46 -0500

    validation

--- simple_crud_front/src/components/SignIn/SignIn.js
+++ simple_crud_front/src/components/SignIn/SignIn.js
@@ -16,14 +16,24 @@ function Login(props) {
   const navigate = useNavigate();
   const [username,setUsername] = useState("");
   const [password,setPassword] = useState("");
-  const [error,setError] = useState(false);
+  const [validated,setValidated] = useState(false);
   const [loading,setLoading] = useState(false);
+  const [error,setError] = useState("");
 
   const goHome= useCallback(() => navigate('/home', {replace: true}), [navigate]);
 
-  const signIn=()=>{
-    setLoading(true);
-    axios.post(`${APIHOST}/users/login`,{
+  const handleSubmit=(event)=>{
+    const form = event.currentTarget;
+
+    event.preventDefault();
+    event.stopPropagation();
+
+    if(form.checkValidity()===false){
+      setError("");
+      setValidated(true);
+    }else{
+      setLoading(true);
+      axios.post(`${APIHOST}/users/login`,{
       username:username,
       password:password
     }).then((response)=>{
@@ -35,32 +45,34 @@ function Login(props) {
         props.setLogged(true);
         goHome();
       }else{
-        setError(true);
+        setError("Username or password incorrect");
       }
-    }).catch((error)=>{
-      console.log(error)
-      setError(true);
+    }).catch((validated)=>{
+      setError("Error");
     }).finally(()=>{
       setLoading(false);
     })
   }
+  }
   return (
     <div className='login container-sm'>
       <Loading show={loading}></Loading>
-      <Form className='rounded-3'>
+      <Form className='rounded-3' noValidate validated={validated} onSubmit={handleSubmit}>
         <div className='mb-5'>
           <h1>Sign in</h1>
         </div>
         <Form.Group className="mb-3">
             <Form.Label>Username</Form.Label>
-            <Form.Control type="input" placeholder="Enter username" onChange={(event)=>setUsername(event.target.value)} />
+            <Form.Control type="input" placeholder="Enter username" onChange={(event)=>setUsername(event.target.value)} required/>
+            <Form.Control.Feedback type="invalid">Please provide a valid username.</Form.Control.Feedback>
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicPassword">
             <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" onChange={(event)=>setPassword(event.target.value)} />
+            <Form.Control type="password" placeholder="Password" onChange={(event)=>setPassword(event.target.value)} required/>
+            <Form.Control.Feedback type="invalid">Please provide a valid password.</Form.Control.Feedback>
         </Form.Group>
-        {error?<div><Form.Text className='text-danger'>Error</Form.Text></div>:null}
-        <Button className='mb-3' variant="primary" onClick={signIn}>Sign in</Button>
+        {error.length>0?<div><Form.Text className='text-danger'>{error}</Form.Text></div>:null}
+        <Button className='mb-3' variant="primary" type='submit'>Sign in</Button>
         <div>
           <Form.Text>Don't have an account? <Link to='/signup' className='text-primary text-decoration-none'>Sign up</Link></Form.Text>
         </div>
--- simple_crud_front/src/components/SignUp/SignUp.js
+++ simple_crud_front/src/components/SignUp/SignUp.js
@@ -22,14 +22,21 @@ function Register() {
   const [last_name,setLast_name]= useState("");
   const [username,setUsername]= useState("");
   const [password,setPassword]= useState("");
-  const [error,setError] = useState(false);
+  const [validated,setValidated] = useState(false);
   const [loading,setLoading] = useState(false);
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
   const [msg,setMsg]=useState("")
 
-  const signUp=()=>{
-    if(name !=="" && last_name !== "" && username !== "" && password !== ""){
+  const handleSubmit=(event)=>{
+    const form = event.currentTarget;
+
+    event.preventDefault();
+    event.stopPropagation();
+
+    if(form.checkValidity()===false){
+      setValidated(true);
+    }else{
       setLoading(true);
       axios.post(`${APIHOST}/users/register`,{
         name:name,
@@ -37,50 +44,47 @@ function Register() {
         username:username,
         password:password
       }).then((response)=>{
-        if(!response.data.succ){
-          setError(true)
-        }else{
+        if(response.data.succ){
           setMsg("User successfully created");
         }
       }).catch((error)=>{
-        setError(true);
         setMsg("Error creating user");
       }).finally(()=>{
         setLoading(false);
         setShow(true);
       })
-    }else{
-      setError(true);
     }
-    
   }
 
   return (
     <div className='register container-sm'>
       <MsgModal show={show} handleClose={handleClose} msg={msg}></MsgModal>
       <Loading show={loading}></Loading>
-      <Form className='rounded-3'>
+      <Form className='rounded-3' noValidate validated={validated} onSubmit={handleSubmit}>
         <div className='mb-5'>
           <h1>Sign Up</h1>
         </div>
         <Form.Group className="mb-3 text-start">
             <Form.Label>Name</Form.Label>
-            <Form.Control type="input" placeholder="Enter name" onChange={(event)=>setName(event.target.value)} />
+            <Form.Control type="input" placeholder="Enter name" onChange={(event)=>setName(event.target.value)} required />
+            <Form.Control.Feedback type="invalid">Please provide a valid name.</Form.Control.Feedback>
         </Form.Group>
         <Form.Group className="mb-3 text-start">
             <Form.Label>Last name</Form.Label>
-            <Form.Control type="input" placeholder="Enter lastname"  onChange={(event)=>setLast_name(event.target.value)} />
+            <Form.Control type="input" placeholder="Enter lastname"  onChange={(event)=>setLast_name(event.target.value)} required />
+            <Form.Control.Feedback type="invalid">Please provide a valid last name.</Form.Control.Feedback>
         </Form.Group>
         <Form.Group className="mb-3 text-start">
             <Form.Label>Username</Form.Label>
-            <Form.Control type="input" placeholder="Enter username"  onChange={(event)=>setUsername(event.target.value)} />
+            <Form.Control type="input" placeholder="Enter username"  onChange={(event)=>setUsername(event.target.value)} required />
+            <Form.Control.Feedback type="invalid">Please provide a valid username.</Form.Control.Feedback>
         </Form.Group>
         <Form.Group className="mb-3 text-start" controlId="formBasicPassword">
             <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Enter password"  onChange={(event)=>setPassword(event.target.value)} />
+            <Form.Control type="password" placeholder="Enter password"  onChange={(event)=>setPassword(event.target.value)} required />
+            <Form.Control.Feedback type="invalid">Please provide a valid password.</Form.Control.Feedback>
         </Form.Group>
-        {error?<div><Form.Text className='text-danger'>Error</Form.Text></div>:null}
-        <Button className='mb-3 text-start' variant="primary" onClick={signUp}>Sign up</Button>
+        <Button className='mb-3 text-start' variant="primary" type='submit' >Sign up</Button>
         <div>
           <Form.Text>Already have an account? <Link to='/login' className='text-primary text-decoration-none'>Sign in</Link></Form.Text>
         </div>
